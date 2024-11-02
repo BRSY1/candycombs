5d5975cb27f41b0d30ee473dac745bc40bc2b5f2
archiekind <archiekind1@gmail.com>
2024-11-02 21:51:15 +0000

    first agent commit

--- agent.py
+++ agent.py
@@ -2,12 +2,16 @@ import pygame
 import random
 import player
 import config
+import tile_map
 
 class Agent(player.Player):
     def __init__(self, image1, image2):
         super().__init__(image1, image2)
-        self.speedx = 0
-        self.speedy = 0
+        self.grid = [[0 for i in range(11)] for j in range(11)]
+
+    def updateGrid(self):
+        self.grid = tile_map.tile_map[self.y-5:self.y+5, self.x-5:self.x+5]
+        print(self.grid)
         
     def update(self):
         self.speedx += random.choice([-1, 0, 1])
