797e4f34533bddabd8c172ced3332de8afcafa01
Jack <wm24026@bristol.ac.uk>
2024-11-02 13:26:59 +0000

    amending map

--- tile_map.py
+++ tile_map.py
@@ -6,9 +6,6 @@ myMap = roomgen.generate_map(100,16,8,8)
 
 tile_map = myMap.grid
 
-<<<<<<< HEAD
-dark_wall = pygame.image.load("assets/tiles/wall_dark.png")
-=======
 floor_cracked = pygame.image.load("assets/tiles/floor_cracked.png")
 floor_cracked = pygame.transform.scale(floor_cracked, (config.TILE_SIZE, config.TILE_SIZE))
 floor_split_cracked = pygame.image.load("assets/tiles/floor_split_cracked.png")
@@ -19,9 +16,8 @@ floor = pygame.image.load("assets/tiles/floor_split.png")
 floor = pygame.transform.scale(floor, (config.TILE_SIZE, config.TILE_SIZE))
 
 dark_wall = pygame.image.load("assets/tiles/dark_wall.png")
->>>>>>> jack
 dark_wall = pygame.transform.scale(dark_wall, (config.TILE_SIZE, config.TILE_SIZE))
-light_wall = pygame.image.load("assets/tiles/wall_light.png")
+light_wall = pygame.image.load("assets/tiles/light_wall.png")
 light_wall = pygame.transform.scale(light_wall, (config.TILE_SIZE, config.TILE_SIZE))
 
 tiles = {
