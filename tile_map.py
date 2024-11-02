6ef282edad3a7f98bba461702375373e2a629228
Jack <wm24026@bristol.ac.uk>
2024-11-02 10:55:14 +0000

    Loading tiles v1

--- tile_map.py
+++ tile_map.py
@@ -1,15 +1,20 @@
 import pygame
 import config
+import roomgen
 
-tile_map = [[1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            ]
+# tile_map = [[1, 1, 1, 1, 1, 1, 1, 1],
+#             [1, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 1, 1, 1, 1, 1, 1, 1],
+#             ]
+
+myMap = roomgen.generate_map(100,16,8,8)
+
+tile_map = myMap.grid
 
 dark_wall = pygame.image.load("assets/tiles/dark_wall.png")
 dark_wall = pygame.transform.scale(dark_wall, (config.TILE_SIZE, config.TILE_SIZE))
@@ -17,6 +22,7 @@ light_wall = pygame.image.load("assets/tiles/light_wall.png")
 light_wall = pygame.transform.scale(light_wall, (config.TILE_SIZE, config.TILE_SIZE))
 
 tiles = {
-    0: light_wall,
-    1: dark_wall
+    'a': light_wall,
+    'b': light_wall,
+    '.': dark_wall
 }
